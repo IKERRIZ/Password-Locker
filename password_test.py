6a507283b6c1e3f242f7a9adf07cdace35cd1a07
IKERRIZ <okothfaith94@gmail.com>
2019-10-06 19:26:05 +0300

    Add set Up method to test if the object is initialized properly

--- password_test.py
+++ password_test.py
@@ -4,3 +4,20 @@ from password_locker import User, Credentials
 class TestUser(Unittest.TestCase):
 
     #Test that defines test cases for the user class bevaviours
+
+    def setUp(self):
+
+        #set up method to run before each test cases
+
+        self.new_user = User("Ikerriz","Kpasloc11","ikerriz@gmail.com")
+
+    def test_init(self)
+
+      #Test case to test if the object is initialized properly
+
+        self.assertEqual(self.new_user.user_name,"Ikerriz")
+        self.assertEqual(self.new_user.password, "Kpasloc11")
+        self.assertEqual(self.new_user.email, "ikerriz@gmail.com")
+
+if __name__ == '__main__':
+    unittest.main()
