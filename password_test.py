ef1ec395a2de849e3b42084dd44f760db37ca4c4
IKERRIZ <okothfaith94@gmail.com>
2019-10-06 22:21:21 +0300

    Test to check if the user can save multiple objects

--- password_test.py
+++ password_test.py
@@ -19,5 +19,26 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.password, "Kpasloc11")
         self.assertEqual(self.new_user.email, "ikerriz@gmail.com")
 
+    def test_save_user(self):
+        '''
+		Test to check if the new users info is saved into the users list
+		'''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+
+
+    def test_save_multiple_user(self):
+        '''
+        Test to check whether we can save multiple user objects.
+        '''
+        
+        self.new_user.user_save()
+        test_user = User("Faith","0727995270","faith@gmail.com")
+        test_user.user_save()
+        self.assertEqual(len(User.user_list),2) 
+
+
+
 if __name__ == '__main__':
     unittest.main()
