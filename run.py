95b9fe469c497ab9dd9749edd7745d9b22551dee
IKERRIZ <okothfaith94@gmail.com>
2019-10-07 10:42:51 +0300

    Add function to create a new user

---
+++ run.py
@@ -0,0 +1,10 @@
+!/usr/bin/env python3.6
+from password_locker import User, Credentials
+
+def create_user(firstname,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(firstname, password)
+    return new_user
+
