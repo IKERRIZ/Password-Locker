c5230b7c80403d460d9d796e7512051843c11a75
IKERRIZ <okothfaith94@gmail.com>
2019-10-07 09:04:25 +0300

    Add a method to check if the user has an existing account

--- password_locker.py
+++ password_locker.py
@@ -14,10 +14,23 @@ class User:
       self.email = email
 
   def user_save(self):
-
        '''
       Function to save a newly created user instance
        '''
+       User.user_list.append(self)
 
-    
-    User.user_list.append(self)
+class Credentials:
+     # class that defines user credential
+
+    credential_list = []
+    user_credential_list = []
+
+    @classmethod
+    def check_user_exist(cls,user_name,password)
+    '''
+    method to check if the user exist from user list
+    '''
+    for user in User.user_list:
+            if user_name == user_name and user.password == password:
+                current_user =user_name
+        return current_user
