f95853e79df881d020225080636c2db61cfc14c4
IKERRIZ <okothfaith94@gmail.com>
2019-10-07 10:50:26 +0300

    Add a function to create a new credential object

--- run.py
+++ run.py
@@ -13,3 +13,17 @@ def save_user(user):
     Function to save a new user
     '''
     user.save_user()
+def authenticate_user(firstname, password):
+    '''
+    Function to authenticate a user
+    '''
+    authenticated_user = Credential.check_user_exist(firstname, password)
+    return authenticated_user
+
+def create_credential(account_name,user_name,password):
+    '''
+    Function to create a new credential object
+    '''
+    new_credential = Credential(account_name,user_name,password)
+    return new_credential
+
